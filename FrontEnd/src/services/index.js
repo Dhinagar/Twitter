50e2a9fcd5d7a0b804f70399be5839e645dad831
Dhinagar <58070266+Dhinagar@users.noreply.github.com>
2022-03-14 14:14:53 +0530

    Add files via upload

---
+++ FrontEnd/src/services/index.js
@@ -0,0 +1,9 @@
+const APIs = {
+    login: `http://localhost:3043/User/login`,
+    searchTweets: `http://localhost:3043/Twitter/All`,
+    loadMore: `http://localhost:3043/Twitter/LoadMore`,
+    count: `http://localhost:3043/Twitter/count`
+}
+
+
+export default APIs
